506e418297e117a2c6ce72db493402b2791433cd
dasjadeleeuw <91197515+dasjadeleeuw@users.noreply.github.com>
2024-05-07 11:00:47 +0200

    Update get_orientations.m

--- helper_functions/get_orientations.m
+++ helper_functions/get_orientations.m
@@ -1,19 +1,19 @@
-function orientations = get_orientations(exp_conditions, ami_umi, delay, total_nr_trials)
+function orientations = get_orientations(exp_conditions, cmi_umi, delay, total_nr_trials)
 % Obtains the to-be-maintained orientation for each individual trial. 
-% Selects either the attended- or unattended orientations. 
+% Selects either the cued or uncued orientations. 
 
 orientations = zeros(1,total_nr_trials);
 
 for i = (1:total_nr_trials)
     cue = exp_conditions(delay,i);                  % delay 1 is first row, delay 2 is second row
 
-    if     ami_umi == 1 && cue == 1                 % if ami, append cued orientation
+    if     cmi_umi == 1 && cue == 1                 % if cmi, append cued orientation
         orientations(i) = exp_conditions(3, i);
-    elseif ami_umi == 1 && cue == 2
+    elseif cmi_umi == 1 && cue == 2
         orientations(i) = exp_conditions(4, i);
-    elseif ami_umi == 2 && cue == 1                 % if umi, append uncued orientation
+    elseif cmi_umi == 2 && cue == 1                 % if umi, append uncued orientation
         orientations(i) = exp_conditions(4, i);
-    elseif ami_umi == 2 && cue == 2
+    elseif cmi_umi == 2 && cue == 2
         orientations(i) = exp_conditions(3, i);
     end
     
